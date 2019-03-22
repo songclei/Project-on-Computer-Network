6b6062c0b17c1d08d461d53ad27f993c03be2c7c
tianye <yet@pku.edu.cn>
2019-03-22 12:30:44 +0800

    fixed untracked files

---
+++ project/config.php
@@ -0,0 +1,11 @@
+<?php
+    // root directory of web server
+    define("WEB_ROOT", "/home/tianye/project/Project-on-Computer-Network/project/web/");
+    
+    // sql connection
+    define("SERVERNAME", "localhost");
+    define("USERNAME", "tianye");
+    define("PASSWORD", "chichichi!");
+    define("DBNAME", "webdb");
+?>
+
