4c1e11e6e84f55a38de63a99dbc90b5f4df2c6b8
songclei <1500017738@pku.edu.cn>
2019-03-26 14:51:38 +0800

    add config.php

---
+++ project/config.php
@@ -0,0 +1,10 @@
+<?php
+// root directory of web server
+    define("WEB_ROOT", "/Users/songchenlei/Desktop/Project-on-Computer-Network/project/web/");
+    
+    // sql connection
+    define("SERVERNAME", "127.0.0.1");
+    define("USERNAME", "root");
+    define("PASSWORD", "chichichi!");
+    define("DBNAME", "webdb");
+?> 
