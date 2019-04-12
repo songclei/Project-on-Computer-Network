690392dfcc25fd182a4838d424e9815f869eea37
Fiona0730pku <1600012731@pku.edu.cn>
2019-04-12 23:32:28 +0800

    增加创建活动界面

--- project/lib/activity.php
+++ project/lib/activity.php
@@ -29,11 +29,52 @@
         $sql = $select . $where . $order;
         
         $conn = new mysqli(SERVERNAME, USERNAME, PASSWORD, DBNAME);
+        if ($conn->connect_error) {
+            die($conn->connect_error);
+        }
         $result = $conn->query($sql);
         $ret = array(
             "num" => $result->num_rows,
             "list" => $result->fetch_all(MYSQLI_ASSOC)
         );
+        $conn->close();
+        return $ret;
+    }
+
+    /**
+    * add a new activity
+    * @param array consist of info
+    * @return mixed
+    */
+
+    function addActivities($params){
+        $ret = array(
+        'status' => 1,
+        'err_msg' => ''
+        );
+
+        $name = $params['name'];
+        $abbr = isset($params['abbr']) ? $params['abbr'] : '';
+        $begin_date = isset($params['begin_date']) ? $params['begin_date'] : '';
+        $end_date = isset($params['end_date']) ? $params['end_date'] : '';
+
+        $conn = new mysqli(SERVERNAME, USERNAME, PASSWORD, DBNAME);
+        $sql = $conn->prepare("insert into activities (name, abbr, begin_date, end_date) values (?, ?, ?, ?)");
+        if ($sql === false) {
+            echo($conn->error);
+            exit();
+            }
+        $sql->bind_param("ssss", $name, $abbr, date('Y-m-d H:i:s',strtotime($begin_date)), date('Y-m-d H:i:s',strtotime($end_date)));
+        
+        $result = $sql->execute();
+        if ($result === true) {
+            $ret['status'] = 0;
+        }
+        else {
+            $ret['err_msg'] = $conn->error;
+            echo($conn->error);
+            ret();
+        }
         $conn->close();
         return $ret;
     }
---
+++ project/web/addActivity.php
@@ -0,0 +1,4 @@
+<?php
+	include_once("../lib/common.php");
+	$smarty3->display("./html/addActivity.html");
+?>
