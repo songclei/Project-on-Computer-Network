868d31f8ef1455ee03a375b561bb604310ed5413
songclei <1500017738@pku.edu.cn>
2019-04-20 17:17:41 +0800

    use SESSION to maintain login status & implement logout

--- project/web/registerAuth.php
+++ project/web/registerAuth.php
@@ -1,11 +1,13 @@
 <?php
     include_once("../lib/common.php");
     include_once("../lib/user.php");
+    session_start();
 
     $param = $_POST;
     $ret = addUser($param);
     
     if ($ret['status'] === 0) {
+        $_SESSION['username'] = $_POST['username'];
         header("Location: ./index.php");
         exit();
     }
